be6ff8eac774414a6d9b666c3a80e46d1e1b17a3
muggy8 <muggy8@gmail.com>
2015-06-24 12:06:56 -0700

    added the ability for checkout box to change where it is on the screen.

--- work/js/homepage.js
+++ work/js/homepage.js
@@ -13,3 +13,13 @@ function submitForm(form){
 	alert("sorry this is just a dummy website for a web design class therefore nothing actually works. sorry if you were actually expectin a real thing...");
 	return false;
 }
+
+function schedulerView(ele){
+	ele.className += " schedulerShow";
+	ele.setAttribute("onclick", "schedulerHide(this)");
+}
+
+function schedulerHide(ele){
+	ele.className = ele.className.replace(" schedulerShow", "");
+	ele.setAttribute("onclick", "schedulerView(this)");
+}
